1442cba527e204becc3882dd909095506db76efd
Ahmed Elghareb <aelghareb.ext@orange.com>
2018-09-17 19:33:10 +0200

    sol

--- 1 - Algorithmic Toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
+++ 1 - Algorithmic Toolbox/week4_divide_and_conquer/1_binary_search/binary_search.cpp
@@ -1,52 +1,55 @@
-#include <iostream>
-#include <cassert>
-#include <vector>
-
-using std::vector;
+#include <bits/stdc++.h>
+using namespace std;
+///////////////////////////////////////
+#define SZ(s) (int)s.size()
+#define ALL(v) v.begin(), v.end()
+#define clr(a,v) memset(a,v,sizeof a)
 
 int binary_search(const vector<int> &a, int x) {
-	int left = 0, right = (int) a.size();
-	int mid = -1;
+	int left = 0, right = (int) a.size()-1;
+	int mid ;
 	while (left < right) {
-		mid = left + (right - left) / 2;
-		if (a[mid] == x) {
-			break;
+
+
+		mid = left + (1+right - left) / 2;
+
+		if(a[mid]<=x){
+			left=mid;
+		}else{
+			right=mid-1;
 		}
-		if (a[mid] > x) {
-			right = mid - 1;
-		}
-		if (a[mid] < x) {
-			left = mid + 1;
-		}
+
 	}
-	if (a[mid] == x) {
-		return mid;
+	if(a[right]==x){
+		return right;
 	}
+
 	return -1;
 }
 
 int linear_search(const vector<int> &a, int x) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
-  }
-  return -1;
+	for (size_t i = 0; i < a.size(); ++i) {
+		if (a[i] == x)
+			return i;
+	}
+	return -1;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
-  }
-  int m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (int i = 0; i < m; ++i) {
-    std::cin >> b[i];
-  }
-  for (int i = 0; i < m; ++i) {
-    //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
-  }
+	int n;
+	std::cin >> n;
+	vector<int> a(n);
+	for (size_t i = 0; i < a.size(); i++) {
+		std::cin >> a[i];
+	}
+	int m;
+	std::cin >> m;
+	vector<int> b(m);
+	for (int i = 0; i < m; ++i) {
+		std::cin >> b[i];
+	}
+	for (int i = 0; i < m; ++i) {
+		//replace with the call to binary_search when implemented
+		std::cout << binary_search(a, b[i]) << ' ';
+	}
 }
--- 1 - Algorithmic Toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
+++ 1 - Algorithmic Toolbox/week4_divide_and_conquer/3_improving_quicksort/sorting.cpp
@@ -1,45 +1,88 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
+#include <bits/stdc++.h>
+using namespace std;
+///////////////////////////////////////
+#define SZ(s) (int)s.size()
+#define ALL(v) v.begin(), v.end()
+#define clr(a,v) memset(a,v,sizeof a)
+////////,///////////////////////////////
+typedef long long ll;
+typedef unsigned long long ull;
+///////////////////////////////////////
+const double EPS = (1e-12);
+const double PI = acos(-1);
+///////////////////////////////////////
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, -1, 1 };
+///////////////////////////////////////
 
-using std::vector;
-using std::swap;
+pair<int, int> partition3(vector<int> &a, int l, int r) {
+	int lt = l;
+	int gt = r;
+	int x = a[l];
 
+	int i = l+1;
+
+	while (i <= gt) {
+		if (a[i] < x) {
+			swap(a[i++], a[lt++]);
+		} else if (a[i] > x) {
+			swap(a[i], a[gt--]);
+		} else if (a[i] == x) {
+			i++;
+		}
+	}
+
+
+	return {lt,gt};
+}
 int partition2(vector<int> &a, int l, int r) {
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
-    }
-  }
-  swap(a[l], a[j]);
-  return j;
+	int x = a[l];
+	int j = l;
+	for (int i = l + 1; i <= r; i++) {
+		if (a[i] <= x) {
+			j++;
+			swap(a[i], a[j]);
+		}
+	}
+	swap(a[l], a[j]);
+	return j;
+}
+
+void randomized_quick_sort3(vector<int> &a, int l, int r) {
+	if (l >= r) {
+		return;
+	}
+
+	int k = l + rand() % (r - l + 1);
+	swap(a[l], a[k]);
+	pair<int, int> m = partition3(a, l, r);
+
+	randomized_quick_sort3(a, l, m.first - 1);
+	randomized_quick_sort3(a, m.second + 1, r);
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
-  if (l >= r) {
-    return;
-  }
+	if (l >= r) {
+		return;
+	}
 
-  int k = l + rand() % (r - l + 1);
-  swap(a[l], a[k]);
-  int m = partition2(a, l, r);
+	int k = l + rand() % (r - l + 1);
+	swap(a[l], a[k]);
+	int m = partition2(a, l, r);
 
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+	randomized_quick_sort(a, l, m - 1);
+	randomized_quick_sort(a, m + 1, r);
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
-  }
-  randomized_quick_sort(a, 0, a.size() - 1);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cout << a[i] << ' ';
-  }
+	int n;
+	std::cin >> n;
+	vector<int> a(n);
+	for (size_t i = 0; i < a.size(); ++i) {
+		std::cin >> a[i];
+	}
+	randomized_quick_sort3(a, 0, a.size() - 1);
+	for (size_t i = 0; i < a.size(); ++i) {
+		std::cout << a[i] << ' ';
+	}
 }
